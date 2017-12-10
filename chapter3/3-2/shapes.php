718beed5af3d029e0518160c12bd5af2b2d07d8f
bryutus <bryutus@gmail.com>
2017-12-10 23:34:39 +0900

    :sparkles: 3.2 Shapes

---
+++ chapter3/3-2/shapes.php
@@ -0,0 +1,19 @@
+<?hh
+
+namespace shapes\sample;
+
+type customer = shape("id" => int, "name" => string);
+
+function create_user(int $id, string $name): customer {
+    return shape("id" => $id, "name" => $name);
+}
+
+function run(): void {
+    echo "==== start shapes sample ====" . PHP_EOL;
+
+    $c = create_user(1, "yone098");
+    var_dump($c["id"]);
+    var_dump($c["name"]);
+}
+
+\shapes\sample\run();
