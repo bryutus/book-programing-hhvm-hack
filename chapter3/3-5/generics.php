2166097053ab5bfa46591515f1075f02c18677ff
bryutus <bryutus@gmail.com>
2017-12-27 18:28:47 +0900

    :sparkles: 3.5 Generics

---
+++ chapter3/3-5/generics.php
@@ -0,0 +1,20 @@
+<?hh
+
+namespace generics\examples;
+
+class Box<T> {
+    public T $value;
+    public function __construct(T $v) {
+        $this->value = $v;
+    }
+}
+
+function run(): void {
+    $a = new Box("A");
+    var_dump($a);
+
+    $b = new Box(100);
+    var_dump($b);
+}
+
+\generics\examples\run();
