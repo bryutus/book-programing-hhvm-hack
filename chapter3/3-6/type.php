9fe1811beb0b45fe07cd332c67fedac84d601abc
bryutus <bryutus@gmail.com>
2018-01-02 07:30:17 +0900

    :sparkles: 3.6 Type Alias

---
+++ chapter3/3-6/type.php
@@ -0,0 +1,25 @@
+<?hh
+
+namespace typealias\samples;
+
+type userId = int;
+newtype password = string;
+
+class User {
+    private userId $uid;
+    private password $pwd;
+
+    public function __construct(userId $userId, password $password) {
+        $this->uid = $userId;
+        $this->pwd = $password;
+    }
+}
+
+function run(): void {
+    $user = new User(100, "qwert");
+    var_dump($user);
+
+    // $user2 = new User("hoge", 123);
+}
+
+\typealias\samples\run();
