8c3f1bf8e2cb81776765877cddccb6930595dfcf
bryutus <bryutus@gmail.com>
2018-01-11 01:54:30 +0900

    :sparkles: 3.7.1 Lambda Design

---
+++ chapter3/3-7/design.php
@@ -0,0 +1,25 @@
+<?hh
+
+namespace lambda\design;
+
+class User {
+    private string $name;
+
+    protected function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    static function get(int $id): User {
+        return new User("User" . strval($id));
+    }
+}
+
+function getUserFromIds(Vector<int> $userids): Vector<User> {
+    return $userids->map($id ==> User::get($id));
+}
+
+function run(): void {
+    var_dump(getUserFromIds(Vector{100, 112, 30, 4}));
+}
+
+\lambda\design\run();
